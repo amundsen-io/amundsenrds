2ca8fb3523c055e1130a91dcd0f7c5bd72bda0df
xuans <xuan_shen@outlook.com>
2021-02-05 13:22:51 -0800

    Change key_len and name_len to handle identifiers in more data sources
    Signed-off-by: xuans <xuan_shen@outlook.com>

--- amundsen_rds/models/base.py
+++ amundsen_rds/models/base.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 metadata = MetaData()
 Base = declarative_base(metadata=metadata)
 
-KEY_LEN = 512
-NAME_LEN = 128
+KEY_LEN = 1024
+NAME_LEN = 256
 URL_LEN = 2048
 PUBLISHED_TAG_LEN = 128
