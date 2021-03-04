84e29414a3cff3396fde1567636a019ecc3c1d8a
Xuan <65048031+crazy-2020@users.noreply.github.com>
2021-03-04 11:00:47 -0800

    fix: Added schema programmatic description model (#5)
    * Added schema programmatic description model
    
    Signed-off-by: xuans <xuan_shen@outlook.com>
    
    * Supported typing for rds model
    
    Signed-off-by: xuans <xuan_shen@outlook.com>
    
    * Renamed rds model variable
    
    Signed-off-by: xuans <xuan_shen@outlook.com>

--- amundsen_rds/models/__init__.py
+++ amundsen_rds/models/__init__.py
@@ -1,5 +1,6 @@
 # Copyright Contributors to the Amundsen project.
 # SPDX-License-Identifier: Apache-2.0
+from typing import Union
 
 from amundsen_rds.models.application import Application, ApplicationTable
 from amundsen_rds.models.badge import Badge
@@ -14,7 +15,9 @@ from amundsen_rds.models.dashboard import (
     DashboardTable, DashboardTag, DashboardTimestamp, DashboardUsage
 )
 from amundsen_rds.models.database import Database
-from amundsen_rds.models.schema import Schema, SchemaDescription
+from amundsen_rds.models.schema import (
+    Schema, SchemaDescription, SchemaProgrammaticDescription
+)
 from amundsen_rds.models.table import (
     Table, TableBadge, TableDescription, TableFollower, TableOwner,
     TableProgrammaticDescription, TableSource, TableTag, TableTimestamp,
@@ -25,44 +28,29 @@ from amundsen_rds.models.updated_timestamp import UpdatedTimestamp
 from amundsen_rds.models.user import User
 
 __all__ = [
-    'Application',
-    'ApplicationTable',
-    'Badge',
-    'Cluster',
-    'TableColumn',
-    'ColumnBadge',
-    'ColumnDescription',
-    'ColumnStat',
-    'Dashboard',
-    'DashboardBadge',
-    'DashboardChart',
-    'DashboardCluster',
-    'DashboardDescription',
-    'DashboardExecution',
-    'DashboardFollower',
-    'DashboardGroup',
-    'DashboardGroupDescription',
-    'DashboardOwner',
-    'DashboardQuery',
-    'DashboardTable',
-    'DashboardTag',
-    'DashboardTimestamp',
-    'DashboardUsage',
-    'Database',
-    'Schema',
-    'SchemaDescription',
-    'Table',
-    'TableBadge',
-    'TableDescription',
-    'TableFollower',
-    'TableOwner',
-    'TableProgrammaticDescription',
-    'TableSource',
-    'TableTag',
-    'TableTimestamp',
-    'TableUsage',
-    'TableWatermark',
-    'Tag',
-    'UpdatedTimestamp',
+    'Application', 'ApplicationTable', 'Badge', 'Cluster',
+    'TableColumn', 'ColumnBadge', 'ColumnDescription', 'ColumnStat',
+    'Dashboard', 'DashboardBadge', 'DashboardChart', 'DashboardCluster',
+    'DashboardDescription', 'DashboardExecution', 'DashboardFollower', 'DashboardGroup',
+    'DashboardGroupDescription', 'DashboardOwner', 'DashboardQuery', 'DashboardTable',
+    'DashboardTag', 'DashboardTimestamp', 'DashboardUsage', 'Database',
+    'Schema', 'SchemaDescription', 'SchemaProgrammaticDescription', 'Table',
+    'TableBadge', 'TableDescription', 'TableFollower', 'TableOwner',
+    'TableProgrammaticDescription', 'TableSource', 'TableTag', 'TableTimestamp',
+    'TableUsage', 'TableWatermark', 'Tag', 'UpdatedTimestamp',
+    'User'
+]
+
+RDSModel = Union[
+    'Application', 'ApplicationTable', 'Badge', 'Cluster',
+    'TableColumn', 'ColumnBadge', 'ColumnDescription', 'ColumnStat',
+    'Dashboard', 'DashboardBadge', 'DashboardChart', 'DashboardCluster',
+    'DashboardDescription', 'DashboardExecution', 'DashboardFollower', 'DashboardGroup',
+    'DashboardGroupDescription', 'DashboardOwner', 'DashboardQuery', 'DashboardTable',
+    'DashboardTag', 'DashboardTimestamp', 'DashboardUsage', 'Database',
+    'Schema', 'SchemaDescription', 'SchemaProgrammaticDescription', 'Table',
+    'TableBadge', 'TableDescription', 'TableFollower', 'TableOwner',
+    'TableProgrammaticDescription', 'TableSource', 'TableTag', 'TableTimestamp',
+    'TableUsage', 'TableWatermark', 'Tag', 'UpdatedTimestamp',
     'User'
 ]
