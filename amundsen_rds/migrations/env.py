05e79baad3e802fcc87b809ef7252495a85cda01
xuans <xuan_shen@outlook.com>
2021-05-28 09:58:17 -0700

    Adjust import
    Signed-off-by: xuans <xuan_shen@outlook.com>

--- amundsen_rds/migrations/env.py
+++ amundsen_rds/migrations/env.py
@@ -6,10 +6,10 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 from amundsen_rds.models.base import Base
 
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
 config = context.config
 
 # Interpret the config file for Python logging.
